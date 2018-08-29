a3d4c1309a123f00497599a490f8e16e9e4bf716
Amil Masic <amil.masic@gmail.com>
2018-08-29 13:24:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,7 +58,7 @@ end
 def over?(board)
   if won?(board) == true || draw?(board) == true || full?(board) == true
     true
-  elsif won?(board) == true && full?(board) == false && draw?(board) == false
+  elsif won?(board) == true && full?(board) == false
     true
   else
     false
